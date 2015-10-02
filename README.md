f56b4234ffbb05aa63b8af5ed62ca749529abd1c
Ferris Nehme <ferris.nehme@gmail.com>
2015-10-02 15:09:02 -0400

    Corrected algorithm, still using inefficient data strcutures. Added a few comments and some notes to the readme.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Capcom-Pro-Tour-Standings-Analyzer
-Given the current standings and the schedule of remaining tournaments, this script will calculate who is guaranteed to be in the Capcom Cup (by points or qualifying), who can still be in the Capcom Cup (by points), and who has no chance of being in the Capcom Cup without winning a premier event.
+Given the current standings and the schedule of remaining tournaments, this script will calculate the worst possible ranking for a player given their current ranking.
+
+Make sure to update the standings file AND schedule file for current results.
+Usage: python main.py
--- main.py
+++ main.py
@@ -1,46 +1,147 @@
-import bisect
+from copy import deepcopy
 
 
 class Player(object):
+
     def __init__(self, name, points):
         self.name = name
         self.points = points
 
 
-def worst_case_rank(ranking_list, player_index, points_left):
-    reference_points = ranking_list[player_index].points
+class Tournament(object):
 
+    """
+    List of points available in a tournament.
+    Popping and removing can be optimized if a more sophisticated ordered
+    data structure is used, but given the small nature of the data set
+    I didn't think it was important.
+    """
+
+    def __init__(self, points):
+        self.points = deepcopy(points)
+        self.points.sort()
+
+    def peek(self):
+        return self.points[-1] if self.points else 0
+
+    def pop(self):
+        return self.points.pop() if self.points else 0
+
+    def find(self, value):
+        """
+        Finds the first element that is greater than value.
+        """
+        for i in range(len(self.points)):
+            if self.points[i] > value:
+                return self.points[i]
+
+    def remove(self, value):
+        """
+        Removes the first element that is greater than value.
+        """
+        for i in range(len(self.points)):
+            if self.points[i] > value:
+                removed = self.points[i]
+                del self.points[i]
+                return removed
+
+    def __str__(self):
+        return str(self.points)
+
+
+class TournamentList(object):
+
+    """
+    Container for tournaments.
+    Can be optimized if using an ordered structure.
+    Currently has to sort after each potentiall disruptive operation.
+    """
+
+    def __init__(self, tournaments):
+        self.tournaments = deepcopy(tournaments)
+        self.sort()
+
+    def peek(self, index=0):
+        if self.tournaments:
+            return self.tournaments[index].peek()
+
+    def sort(self):
+        """
+        Sorts the tournaments but the highest 1st place points available.
+        NOT by total points.
+        """
+        self.tournaments.sort(key=lambda tourney: tourney.peek())
+
+    def pop(self, index=0):
+        if self.tournaments:
+            result = self.tournaments[index].pop()
+            self.sort()
+            return result
+
+    def remove(self, value, index=0):
+        """
+        Removes the smallest prize greater than value.
+        """
+        if not self.tournaments:
+            raise ValueError("Nothing in list")
+
+        smallest = self.tournaments[index]
+        smallest_value = smallest.find(value)
+
+        for tourney in self.tournaments[index:]:
+            candidate = tourney.find(value)
+            if candidate < smallest_value:
+                smallest = tourney
+                smallest_value = candidate
+        removed = smallest.remove(value)
+        self.sort()
+        return removed
+
+    def __str__(self):
+        result = ''
+        for i in self.tournaments:
+            result += str(i)
+            result += '\n'
+        return result
+
+
+def worst_case_rank(ranking_list, player_index, tournaments):
+    """
+    Returns the worst possible ranking for a player by the end of
+    the year given their current ranking.
+    """
+    player = ranking_list[player_index]
     worst_rank = player_index
-    prev_points = reference_points
-    points_left_index = len(points_left)-1
-    max_points_gain = points_left[points_left_index]
-    for player in ranking_list[player_index+1:]:
-        # list needs to be in descending order
-        if player.points > prev_points:
-            raise ValueError("ranking_list not sorted"
-                             "or sorted in wrong order.")
-        prev_points = player.points
-        
-        # It is possible to be taken over by someone with equal points
-        # Whatever tie breaker system they have, assume the worst outcome
-        # for the player in question
-        points_to_surpass = reference_points - player.points
 
-        if points_to_surpass <= max_points_gain:
-            points_left_index -= 1
-            max_points_gain = points_left[points_left_index]
-            worst_rank += 1
-        else:
-            return worst_rank
-        if points_left_index < 0:
+    for other in ranking_list[player_index+1:]:
+        surpass = player.points - other.points
+        points = other.points
+
+        count = 0
+        while (
+            count < len(tournaments.tournaments) and
+            tournaments.peek(count) + points < player.points
+        ):
+            points += tournaments.pop(count)
+            count += 1
+
+        if count == len(tournaments.tournaments):
             return worst_rank
 
+        surpass = player.points - points
+        removed = tournaments.remove(surpass, count)
+        points += removed
+        worst_rank += 1
+        print(
+            other.name, other.points, 'surpasses',
+            player.name, 'by gaining', points-other.points
+        )
     return worst_rank
-        
-    
+
+
 if __name__ == "__main__":
     ranking = []
-    with open("October 2, 2015 Standings.txt", 'rt') as fp:
+    with open("standings.txt", 'rt') as fp:
         for line in fp:
             data = line.split(' ')
             if len(data) > 2 and data[2].strip() == 'Qualified':
@@ -49,31 +150,28 @@ if __name__ == "__main__":
             points = int(data[1])
             ranking.append(Player(name, points))
 
-
     tournament_tiers = {}
     with open("Ranking Point Matrix.txt", 'rt') as fp:
         for line in fp:
             data = line.split(' ')
             tier = data[0].lower().strip()
             tournament_tiers[tier] = []
-            for value in data[2:]:  #Skip 1st place
+            if tier == 'premier' or tier == 'evoloution':
+                start = 2
+            else:
+                start = 1
+            for value in data[start:]:  # Skip 1st place
                 tournament_tiers[tier].append(int(value))
 
-    points_left = []
+    tournaments = []
     with open("schedule.txt", 'rt') as fp:
         for tier in fp:
             tier = tier.lower().strip()
-            points_left.extend(tournament_tiers[tier])
+            tournaments.append(Tournament(tournament_tiers[tier]))
 
-    points_left.sort()
-
-
-    # Can do a binary search here
-    # But who cares on a list this size...
-    count = 0
-    for i in range(len(ranking)):
-        if worst_case_rank(ranking, i, points_left) < 16:
-            print(ranking[i].name)
-            count += 1
-    print(count, "unqualified players are guaranteed a spot.")
-
+    tlist = TournamentList(tournaments)
+    index = 0
+    print(
+        "The lowest ranking", ranking[index].name,
+        "can get is", worst_case_rank(ranking, index, tlist)+1
+    )
